8707a72f8bb9340bd965bdc1af2ffe718dcd4520
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-13 19:36:06 +0530

    configure chaotic aur

--- setup-arch.sh
+++ setup-arch.sh
@@ -1,14 +1,5 @@
 
-
-
-if ! test -f "/etc/pacman.d/chaotic-mirrorlist"
-then
-    echo -e "Configuring Chaotic-AUR - https://aur.chaotic.cx/docs..."
-    sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
-    sudo pacman-key --lsign-key 3056513887B78AEB
-    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-    sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-fi
+pacman-configure-chaotic-aur
 
 echo -e "Updating /etc/pacman.conf ..."
 sudo sed -i "/^#Color/c\Color\nILoveCandy
@@ -22,4 +13,25 @@ sudo pacman -Fy
 
 echo -e "Installing fonts..."
 sudo pacman -Sy noto-fonts noto-fonts-emoji ttf-liberation ttf-dejavu ttf-roboto
+
+pacman-configure-chaotic-aur()
+{
+    if [ ! -f /etc/pacman.d/chaotic-mirrorlist ]
+    then
+        echo -e "Configuring Chaotic-AUR - https://aur.chaotic.cx/docs..."
+        sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
+        sudo pacman-key --lsign-key 3056513887B78AEB
+        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+
+        if ! grep --quiet -x '\[chaotic-aur\]' '/etc/pacman.conf' && grep --quiet -x 'Include = /etc/pacman.d/chaotic-mirrorlist' '/etc/pacman.conf'
+        then
+            sudo echo -e "#Chaotic-AUR" >> /etc/pacman.conf
+            sudo echo -e "" >> /etc/pacman.conf
+            sudo echo -e "[chaotic-aur]" >> /etc/pacman.conf
+            sudo echo -e "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
+        fi
+    fi
+}
 
