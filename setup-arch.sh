55c9dc43c666b94b1c6da4e96fca8e3175612539
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-13 21:51:31 +0530

    updated

--- setup-arch.sh
+++ setup-arch.sh
@@ -1,7 +1,16 @@
 
+
+
+echo -e "Installing fonts..."
+sudo pacman -Sy --needed noto-fonts noto-fonts-emoji ttf-liberation ttf-dejavu ttf-roboto ttf-ubuntu-font-family ttf-jetbrains-mono-nerd
+
+echo -e "Installing some needed stuffs..."
+sudo pacman -Sy --needed pacman-contrib firefox base-devel git
+
+
 pacman-configure-chaotic-aur
 
-echo -e "Updating /etc/pacman.conf ..."
+echo -e "Doing some cool stuffs in /etc/pacman.conf ..."
 sudo sed -i "/^#Color/c\Color\nILoveCandy
     /^#VerbosePkgLists/c\VerbosePkgLists
     /^#ParallelDownloads/c\ParallelDownloads = 5" /etc/pacman.conf
@@ -10,9 +19,8 @@ sudo sed -i '/^#\[multilib\]/,+1 s/^#//' /etc/pacman.conf
 sudo pacman -Syyu
 sudo pacman -Fy
 
-
-echo -e "Installing fonts..."
-sudo pacman -Sy noto-fonts noto-fonts-emoji ttf-liberation ttf-dejavu ttf-roboto
+echo -e "Installing some more needed stuffs..."
+sudo pacman -Sy --needed yay rate-mirrors
 
 pacman-configure-chaotic-aur()
 {
@@ -21,17 +29,16 @@ pacman-configure-chaotic-aur()
         echo -e "Configuring Chaotic-AUR - https://aur.chaotic.cx/docs..."
         sudo pacman-key --recv-key 3056513887B78AEB --keyserver keyserver.ubuntu.com
         sudo pacman-key --lsign-key 3056513887B78AEB
-        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
-        sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+        sudo pacman -Uy 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
+        sudo pacman -Uy 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+    fi
 
-
-        if ! grep --quiet -x '\[chaotic-aur\]' '/etc/pacman.conf' && grep --quiet -x 'Include = /etc/pacman.d/chaotic-mirrorlist' '/etc/pacman.conf'
-        then
-            sudo echo -e "#Chaotic-AUR" >> /etc/pacman.conf
-            sudo echo -e "" >> /etc/pacman.conf
-            sudo echo -e "[chaotic-aur]" >> /etc/pacman.conf
-            sudo echo -e "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
-        fi
+    if ! grep --quiet -x '\[chaotic-aur\]' '/etc/pacman.conf'
+    then
+        sudo echo -e "#Chaotic-AUR" >> /etc/pacman.conf
+        sudo echo -e "" >> /etc/pacman.conf
+        sudo echo -e "[chaotic-aur]" >> /etc/pacman.conf
+        sudo echo -e "Include = /etc/pacman.d/chaotic-mirrorlist" >> /etc/pacman.conf
     fi
 }
 
