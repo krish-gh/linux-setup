eff1e43df92ee56b33a24aa9aaf17bb79be639da
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-17 09:56:18 +0530

    updated

--- scripts/setup-arch.sh
+++ scripts/setup-arch.sh
@@ -179,7 +179,7 @@ setup_gtk() {
 
 setup_gnome() {
     echo -e "Configuring gnome stuffs..."
-    sudo pacman -Rns --noconfirm snapshot gnome-calculator gnome-calendar gnome-clocks gnome-connections gnome-contacts gnome-disk-utility baobab simple-scan gnome-maps gnome-music gnome-tour totem gnome-weather epiphany gnome-user-docs yelp
+    sudo pacman -Rns --noconfirm snapshot gnome-calculator gnome-calendar gnome-clocks gnome-connections gnome-contacts baobab simple-scan gnome-maps gnome-music gnome-tour totem gnome-weather epiphany gnome-user-docs yelp
     sudo pacman -S --noconfirm --needed gnome-themes-extra gnome-menus gnome-tweaks gnome-shell-extensions python-pipx
     
     gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
