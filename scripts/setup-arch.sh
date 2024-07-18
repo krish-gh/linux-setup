95723ddba738ffa2103f91859b5e4fca293479fa
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-18 09:22:52 +0530

    updated

--- scripts/setup-arch.sh
+++ scripts/setup-arch.sh
@@ -179,7 +179,11 @@ setup_gtk() {
 
 setup_gnome() {
     echo -e "Configuring gnome stuffs..."
-    sudo pacman -Rns --noconfirm snapshot gnome-calculator gnome-calendar gnome-clocks gnome-connections gnome-contacts baobab simple-scan gnome-maps gnome-music gnome-tour gnome-weather epiphany totem gnome-user-docs yelp gedit gnome-terminal
+
+    pkgtoremove=(snapshot gnome-calculator gnome-calendar gnome-clocks gnome-connections gnome-contacts baobab simple-scan gnome-maps gnome-music gnome-tour gnome-weather epiphany totem gnome-user-docs yelp gedit gnome-terminal)
+    #doing removing in loop to avoid abort in case something is not installed
+    for i in ${pkgtoremove[@]}; do sudo pacman -Rns --noconfirm $i; done
+    
     sudo pacman -S --noconfirm --needed gnome-themes-extra gnome-menus gnome-tweaks gnome-shell-extensions gnome-console gnome-text-editor python-pipx
     
     gsettings set org.gnome.desktop.wm.preferences button-layout ':minimize,maximize,close'
