95feef62757983add1aa0a67ff197fbc0fcb1b65
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-19 09:59:22 +0530

    wezterm

---
+++ home/.wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Catppuccin Mocha'
+config.font_size = 16.0
+config.hide_tab_bar_if_only_one_tab = true
+config.audible_bell = 'Disabled'
+
+return config
