d7e242fb8c776c754581c7ac62ec8da113473995
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-19 10:15:12 +0530

    updated

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+config.default_prog = { "/bin/bash" }
 config.color_scheme = 'Catppuccin Mocha'
 config.font_size = 16.0
 config.hide_tab_bar_if_only_one_tab = true
