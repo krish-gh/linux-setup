862100a69df0e24a39727c6f30904fd18632734c
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-19 11:26:37 +0530

    updated

--- home/.wezterm.lua
+++ home/.wezterm.lua
@@ -2,9 +2,10 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
+--config.enable_wayland = false
 config.default_prog = { "/bin/bash" }
 config.color_scheme = 'Catppuccin Mocha'
-config.font_size = 16.0
+config.font_size = 14.0
 config.hide_tab_bar_if_only_one_tab = true
 config.audible_bell = 'Disabled'
 
