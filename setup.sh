ed022ab2384768a8a68cdc00bc80c2e7a7c2b13c
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-21 03:35:04 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget -nd -np https://raw.githubusercontent.com/krish-gh/linux-setup/main/scripts/setup-main.sh
-chmod +x setup-arch.sh
+chmod +x setup-main.sh
 ./setup-main.sh | tee setup.log
-rm -rf setup-arch.sh .wget-hsts
+rm -rf setup-main.sh .wget-hsts
