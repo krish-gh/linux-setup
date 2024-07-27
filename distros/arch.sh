7f9401838e3cc1f67a35c99c86750941273ba10d
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-27 22:23:42 +0530

    updated

--- distros/arch.sh
+++ distros/arch.sh
@@ -5,6 +5,4 @@ REFRESH_CMD="sudo pacman -Syu --noconfirm"
 INSTALL_CMD="sudo pacman -S --noconfirm --needed"
 UNINSTALL_CMD="sudo pacman -Rns --noconfirm"
 
-declare -A pkgmap
-
 echo -e "Done arch.sh..."
