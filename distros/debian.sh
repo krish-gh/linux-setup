880919bcdcf9ba16c46c1991276283023e9b3a24
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-28 01:20:11 +0530

    updated

--- distros/debian.sh
+++ distros/debian.sh
@@ -6,5 +6,10 @@ INSTALL_CMD="sudo apt install -y"
 UNINSTALL_CMD="sudo apt purge --ignore-missing --auto-remove -y"
 
 REQUIREMENTS="curl build-essential"
+SYSTEM_PACKAGES_TO_INSTALL="mesa-vulkan-drivers firmware-sof-signed alsa-{firmware-loaders,oss,utils} fprintd libpam-fprintd"
+INTEL_PACKAGES_TO_INSTALL="intel-media-va-driver-non-free va-driver-all"
+VMWARE_PACKAGES_TO_INSTALL="xserver-xorg-video-vmware open-vm-tools-desktop"
+VBOX_PACKAGES_TO_INSTALL="virtualbox-guest-x11"
+HYPERV_PACKAGES_TO_INSTALL=""
 
 echo -e "Done debian.sh..."
