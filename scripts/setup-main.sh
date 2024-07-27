14350275edcb1a5c80c7439361841cfb4d603274
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-28 03:03:00 +0530

    updated

--- scripts/setup-main.sh
+++ scripts/setup-main.sh
@@ -14,16 +14,10 @@ download_content() {
     curl "$1"
 }
 
-# Check if the distro is (based on) Arch Linux - temporary until other distro support is added
-if ! command_exists pacman; then
-    echo "You are not running an Arch-based Linux distrbution..."
-    exit 1
-fi
-
 DISTRO=''
 command_exists pacman && DISTRO=arch
 command_exists apt && DISTRO=debian
-command_exists dnf && DISTRO=fedora
+#command_exists dnf && DISTRO=fedora
 
 if [[ $DISTRO == '' ]]; then
     echo "You are not running supported Linux distrbution..."
@@ -38,19 +32,19 @@ REFRESH_CMD="" #override from distro specific script
 INSTALL_CMD="" #override from distro specific script
 UNINSTALL_CMD="" #override from distro specific script
 
-REQUIREMENTS="curl base-devel xorg-xrdb"
-SYSTEM_PACKAGES_TO_INSTALL="vulkan-{mesa-layers,swrast,icd-loader} sof-firmware alsa-{firmware,oss,plugins,utils} fprintd"
-INTEL_PACKAGES_TO_INSTALL="intel-media-driver vulkan-intel"
-VMWARE_PACKAGES_TO_INSTALL="xf86-video-vmware xf86-input-vmmouse gtkmm gtkmm3 open-vm-tools"
-VBOX_PACKAGES_TO_INSTALL="virtualbox-guest-utils"
-HYPERV_PACKAGES_TO_INSTALL="hyperv"
-FONTS_TO_INSTALL="noto-fonts{,-extra,-emoji} ttf-{liberation,dejavu,roboto,ubuntu-font-family,nerd-fonts-symbols-mono,jetbrains-mono}"
-TERM_PACKAGES_TO_INSTALL="diffutils bash-completion nano-syntax-highlighting starship fastfetch xclip wl-clipboard neovim xterm"
-APP_PACKAGES_TO_INSTALL="pacman-contrib firefox{,-i18n-en-gb,-i18n-en-us} gnome-keyring seahorse vlc"
-DEV_PACKAGES_TO_INSTALL="git github-cli shfmt meld"
-GTK_PACKAGES_TO_INSTALL="kvantum-qt5 qt{5,6}-wayland qt{5,6}ct"
-GNOME_PACKAGES_TO_INSTALL="gnome-{themes-extra,menus,tweaks,shell-extensions,console,text-editor} python-nautilus python-pipx"
-PACKAGES_TO_REMOVE="snapshot baobab simple-scan epiphany totem gedit vim neofetch gnome-{calculator,calendar,characters,clocks,connections,contacts,font-viewer,maps,music,nettool,power-manager,screenshot,tour,weather,user-docs,terminal} yelp"
+REQUIREMENTS="" #override from distro specific script
+SYSTEM_PACKAGES_TO_INSTALL="" #override from distro specific script
+INTEL_PACKAGES_TO_INSTALL="" #override from distro specific script
+VMWARE_PACKAGES_TO_INSTALL="" #override from distro specific script
+VBOX_PACKAGES_TO_INSTALL="" #override from distro specific script
+HYPERV_PACKAGES_TO_INSTALL="" #override from distro specific script
+FONTS_TO_INSTALL="" #override from distro specific script
+TERM_PACKAGES_TO_INSTALL="" #override from distro specific script
+APP_PACKAGES_TO_INSTALL="" #override from distro specific script
+DEV_PACKAGES_TO_INSTALL="" #override from distro specific script
+GTK_PACKAGES_TO_INSTALL="" #override from distro specific script
+GNOME_PACKAGES_TO_INSTALL="" #override from distro specific script
+PACKAGES_TO_REMOVE="" #override from distro specific script
 
 TERMINAL_TO_INSTALL=kitty
 GUI_TEXT_EDITOR="" #override from desktop specific script
