4bbdc8ebc74afb27265e17b6d9ab2aac9d7d2459
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-28 00:58:17 +0530

    updated

--- distros/debian.sh
+++ distros/debian.sh
@@ -5,4 +5,6 @@ REFRESH_CMD="sudo apt update && sudo apt full-upgrade -y"
 INSTALL_CMD="sudo apt install -y"
 UNINSTALL_CMD="sudo apt purge --ignore-missing --auto-remove -y"
 
+REQUIREMENTS="curl build-essential"
+
 echo -e "Done debian.sh..."
