d210eb3d7daf6326bd5fedfb70af3205861f0d79
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-27 16:48:55 +0530

    updated

--- scripts/setup-main.sh
+++ scripts/setup-main.sh
@@ -34,14 +34,14 @@ TERMINAL_TO_INSTALL=kitty
 GUI_TEXT_EDITOR="OVERRIDE WITH DESKTOP SPECIFIC EDITOR"
 
 # override with distro and desktop specific stuffs
-curl -o $DISTRO.sh ${BASE_REPO_URL}distros/$DISTRO.sh
+curl -o ~/$DISTRO.sh ${BASE_REPO_URL}distros/$DISTRO.sh
 # shellcheck disable=SC1090
-source $DISTRO.sh
-#rm -rf $DISTRO.sh
-curl -o "$DESKTOP_SESSION".sh ${BASE_REPO_URL}desktop/"$DESKTOP_SESSION".sh
+source ~/$DISTRO.sh
+#rm -rf ~/$DISTRO.sh
+curl -o ~/"$DESKTOP_SESSION".sh ${BASE_REPO_URL}desktop/"$DESKTOP_SESSION".sh
 # shellcheck disable=SC1090
-source "$DESKTOP_SESSION".sh
-#rm -rf "$DESKTOP_SESSION".sh
+source ~/"$DESKTOP_SESSION".sh
+#rm -rf ~/"$DESKTOP_SESSION".sh
 
 refresh_package_sources() {
     $REFRESH_CMD
