226d95227ad751ea540be1e8e494fe043931c9e2
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-28 12:24:30 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # shellcheck disable=SC2046
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/krish-gh/linux-setup/main/scripts/setup-main.sh?$(date +%s))"
+eval "$(curl -fsSL https://raw.githubusercontent.com/krish-gh/linux-setup/main/scripts/setup-main.sh?$(date +%s))"
