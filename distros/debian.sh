b9ad313110cdc2a5897f4918cba48b953985d08e
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-28 11:52:29 +0530

    updated

--- distros/debian.sh
+++ distros/debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # shellcheck disable=SC2034
-REFRESH_CMD="sudo apt update && sudo apt full-upgrade -y"
+REFRESH_CMD='sudo apt update && sudo apt full-upgrade -y'
 INSTALL_CMD="sudo apt install -y"
 UNINSTALL_CMD="sudo apt purge --ignore-missing --auto-remove -y"
 
