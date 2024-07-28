2e0dc8de64bf8123a63a979e7a8c7ea99be1fb41
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-28 10:56:20 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sh -c "$(curl -H 'Cache-Control: no-cache, no-store' -fsSL https://raw.githubusercontent.com/krish-gh/linux-setup/main/scripts/setup-main.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/krish-gh/linux-setup/main/scripts/setup-main.sh)"
