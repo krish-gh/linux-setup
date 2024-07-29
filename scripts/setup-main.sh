1e0b02cf885109b3ef6ccb76f722c47855856b8f
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-29 10:52:36 +0530

    updated

--- scripts/setup-main.sh
+++ scripts/setup-main.sh
@@ -338,12 +338,14 @@ setup_apt() {
     install_pkgs "nala wget gpg apt-transport-https"
 
     # vscode
-    wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-    sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-    echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
-    rm -f packages.microsoft.gpg
-    refresh_package_sources
-    install_pkgs code
+    if [[ ! -f /etc/apt/sources.list.d/vscode.list ]]; then
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+        echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+        rm -f packages.microsoft.gpg
+        refresh_package_sources
+        install_pkgs code
+    fi
 }
 
 setup_gtk() {
