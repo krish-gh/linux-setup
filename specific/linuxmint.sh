950f51292a83c80ac10c7c817e86c38561236cbd
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-30 21:29:17 +0530

    updated

---
+++ specific/linuxmint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# shellcheck disable=SC2035
+sudo apt-get purge --ignore-missing --auto-remove -y celluloid hypnotix mintchat *thunderbird* *timeshift* *transmission* warpinator webapp-manager
