dc68cfbb5da15ed8edd0678b7c3bb60f50cd7c69
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-31 11:49:32 +0530

    updated

--- specific/linuxmint.sh
+++ specific/linuxmint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# shellcheck disable=SC2035
-sudo apt-get purge --ignore-missing --auto-remove -y celluloid hypnotix mintchat *thunderbird* *timeshift* *transmission* warpinator webapp-manager
+uninstall_pkgs "celluloid hypnotix mintchat *thunderbird* *timeshift* *transmission* warpinator webapp-manager"
 
 echo -e "Done linuxmint.sh..."
