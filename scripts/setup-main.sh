22dcaafc1edb4326fc5f3a25d05af2910c92d6a1
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-31 10:09:45 +0530

    updated

--- scripts/setup-main.sh
+++ scripts/setup-main.sh
@@ -455,7 +455,7 @@ setup_apt() {
     if [[ ! -f /etc/apt/sources.list.d/vscode.list ]]; then
         wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
         sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-        echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
         rm -f packages.microsoft.gpg .wget-hsts
     fi
 
