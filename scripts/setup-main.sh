3f838d8734f6b5f8f583f35a4f591817be783c67
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-31 10:19:15 +0530

    updated

--- scripts/setup-main.sh
+++ scripts/setup-main.sh
@@ -450,13 +450,22 @@ setup_pacman() {
 setup_apt() {
     echo -e "Setting up apt..."
     install_pkgs "nala wget gpg apt-transport-https"
+    sudo mkdir -p -m 755 /etc/apt/keyrings
 
     # vscode
     if [[ ! -f /etc/apt/sources.list.d/vscode.list ]]; then
-        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
-        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
+        wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg 
+        sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg 
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null 
         rm -f packages.microsoft.gpg .wget-hsts
+    fi
+
+    # github
+    if [[ ! -f /etc/apt/sources.list.d/github-cli.list ]]; then
+        wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+        && sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+        && echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null 
+        rm -f .wget-hsts
     fi
 
     # add some ppa if ubuntu based
@@ -466,7 +475,7 @@ setup_apt() {
     fi
 
     refresh_package_sources
-    install_pkgs "code qt6-style-kvantum"
+    install_pkgs "code gh qt6-style-kvantum"
 }
 
 echo -e "Removing not needed packages..."
