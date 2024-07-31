aabe70ddcc1fa1f44fbb2e7120b75a202a0533e3
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-07-31 09:40:08 +0530

    updated

--- scripts/setup-main.sh
+++ scripts/setup-main.sh
@@ -457,9 +457,10 @@ setup_apt() {
         sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
         echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list >/dev/null
         rm -f packages.microsoft.gpg .wget-hsts
-        refresh_package_sources
-        install_pkgs code
     fi
+
+    refresh_package_sources
+    install_pkgs code
 }
 
 echo -e "Removing not needed packages..."
