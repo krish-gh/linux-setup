76cd917fb7da931a61391ba9a8f172bcc6f3429d
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-03 13:08:06 +0530

    updated

--- scripts/setup-main.sh
+++ scripts/setup-main.sh
@@ -64,7 +64,7 @@ GNOME_EXT_MGR_PKG=""            #override from DISTRO_TYPE specific script
 CINNAMON_PACKAGES_TO_INSTALL="" #override from DISTRO_TYPE specific script
 PACKAGES_TO_REMOVE=""           #override from DISTRO_TYPE specific script
 
-TERMINAL_TO_INSTALL=kitty
+TERMINAL_TO_INSTALL=none
 GUI_TEXT_EDITOR="" #override from desktop specific script
 
 # arg1 = destination path, arg2 = source path
