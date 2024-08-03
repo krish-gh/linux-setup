44426abc03cb8fdd37048c83907462ddabde672e
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-03 22:25:22 +0530

    updated

--- distros/debian.sh
+++ distros/debian.sh
@@ -24,7 +24,12 @@ PACKAGES_TO_REMOVE="baobab caribou celluloid cheese drawing epiphany evolution g
 
 setup_apt() {
     echo -e "Setting up apt..."
-    install_pkgs "nala wget gpg apt-transport-https"
+    install_pkgs "software-properties-common nala wget gpg apt-transport-https"
+
+    if [[ $DIST_ID == *debian* ]]; then
+        sudo apt-add-repository contrib non-free non-free-firmware -y
+    fi
+
     sudo mkdir -p -m 755 /etc/apt/keyrings
 
     # microsoft
