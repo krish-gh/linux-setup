93bab106714ab2ce5c256a9cd9d8d4c033adaf68
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-04 14:06:26 +0530

    updated

--- specific/ubuntu.sh
+++ specific/ubuntu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 setup_ubuntu() {
-    sudo snap remove firefox
-    printf 'Package: * \nPin: origin packages.mozilla.org \nPin-Priority: 1000 \n' | sudo tee /etc/apt/preferences.d/mozilla
+    sudo snap remove firefox # remove snap version of firefox
     echo -e "Done ubuntu..."
 }
