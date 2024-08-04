140a5b58fa44c09fcf137d5f38d6b3ea828b6c32
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-04 17:02:20 +0530

    updated

--- specific/ubuntu.sh
+++ specific/ubuntu.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 setup_ubuntu() {
-    echo -e "Replacing snap version of firefox with native one..."
-    sudo install -d -m 0755 /etc/apt/keyrings
-    wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
-    echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list >/dev/null
-    printf 'Package: * \nPin: origin packages.mozilla.org \nPin-Priority: 1000 \n' | sudo tee /etc/apt/preferences.d/mozilla
-    sudo apt-get update && sudo snap remove firefox && uninstall_pkgs "*firefox*"
+    # echo -e "Replacing snap version of firefox with native one..."
+    # sudo install -d -m 0755 /etc/apt/keyrings
+    # wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
+    # echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list >/dev/null
+    # printf 'Package: * \nPin: origin packages.mozilla.org \nPin-Priority: 1000 \n' | sudo tee /etc/apt/preferences.d/mozilla
+    # sudo apt-get update && sudo snap remove firefox && uninstall_pkgs "*firefox*"
+    echo
 }
 
 echo -e "Done ubuntu.sh..."
