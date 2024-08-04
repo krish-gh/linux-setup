a92da1d78116ca377e6145f3ce74c696c4b533aa
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-04 14:08:23 +0530

    updated

--- distros/debian.sh
+++ distros/debian.sh
@@ -53,7 +53,6 @@ setup_apt() {
     # mozilla
     wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc >/dev/null
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee /etc/apt/sources.list.d/mozilla.list >/dev/null
-    printf 'Package: * \nPin: origin packages.mozilla.org \nPin-Priority: 1000 \n' | sudo tee /etc/apt/preferences.d/mozilla
 
     rm -f .wget-hsts
 
--- specific/ubuntu.sh
+++ specific/ubuntu.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 setup_ubuntu() {
-    sudo snap remove firefox # remove snap version of firefox
+    echo -e "Replacing snap version of firefox with native one..."
+    sudo snap remove firefox
+    printf 'Package: * \nPin: origin packages.mozilla.org \nPin-Priority: 1000 \n' | sudo tee /etc/apt/preferences.d/mozilla
     echo -e "Done ubuntu..."
 }
