8fe8d609034ce255fb4164e386a49e2b1511615a
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-06 02:26:37 +0530

    updated

--- distros/fedora.sh
+++ distros/fedora.sh
@@ -36,6 +36,10 @@ setup_dnf() {
     sudo dnf config-manager --disable *nvidia*
     sudo dnf config-manager --disable *steam*
 
+    # microsoft
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+
 }
 
 echo -e "Done fedora.sh..."
