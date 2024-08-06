ea7a1c3cc0d69e880b45f71191100a0eae61d53e
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-06 13:17:12 +0530

    updated

--- distros/fedora.sh
+++ distros/fedora.sh
@@ -8,13 +8,13 @@ UNINSTALL_CMD="sudo dnf autoremove -y"
 
 REQUIREMENTS="curl wget2-wget unzip xrdb dconf"
 SYSTEM_PACKAGES_TO_INSTALL="mesa-vulkan-drivers vulkan-loader alsa-{firmware,sof-firmware,plugins-oss,utils} fprintd"
-INTEL_PACKAGES_TO_INSTALL="mesa-va-drivers intel-media-driver"
+INTEL_PACKAGES_TO_INSTALL="intel-media-driver"
 VMWARE_PACKAGES_TO_INSTALL="xorg-x11-drv-vmware open-vm-tools-desktop"
 VBOX_PACKAGES_TO_INSTALL="virtualbox-guest-additions"
 HYPERV_PACKAGES_TO_INSTALL="hyperv-daemons hyperv-tools"
 FONTS_TO_INSTALL="{liberation,google-noto}-fonts-common google-noto-{emoji,color-emoji}-fonts jetbrains-mono-fonts"
 TERM_PACKAGES_TO_INSTALL="bash-completion nano starship fastfetch"
-APP_PACKAGES_TO_INSTALL="firefox{,-langpacks} gnome-keyring seahorse vlc"
+APP_PACKAGES_TO_INSTALL="firefox{,-langpacks} mozilla-openh264 gnome-keyring seahorse vlc"
 DEV_PACKAGES_TO_INSTALL="git make automake gcc gcc-c++ shfmt diffutils meld gh code"
 GTK_PACKAGES_TO_INSTALL="gnome-themes-extra"
 QT_PACKAGES_TO_INSTALL="kvantum kvantum-qt5 qt{5,6}-qtwayland qt{5,6}ct"
@@ -31,7 +31,10 @@ setup_dnf() {
     sudo dnf config-manager --enable fedora-cisco-openh264
     sudo dnf update -y @core
     sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
-    sudo dnf update -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin    
+    sudo dnf swap libva-intel-media-driver intel-media-driver --allowerasing
+    sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld --allowerasing
+    sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld --allowerasing
+    sudo dnf update -y @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
 
     echo -e "Disabling some not needed repos..."
     sudo dnf config-manager --disable *PyCharm*
