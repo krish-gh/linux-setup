3a039b4dff254340f8874021e8e2d3529adcb655
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-09 10:34:29 +0530

    updated

--- specific/linuxmint.sh
+++ specific/linuxmint.sh
@@ -2,7 +2,8 @@
 
 setup_linuxmint() {
     uninstall_pkgs "hypnotix mintchat pix* sticky thingy *timeshift* warpinator webapp-manager"
-    sudo apt-get install mint-meta-codecs mint-meta-"$DESKTOP" # just ensuring this meta package was not uninstalled, it will wait for confirmation if it was
+    sudo apt-get install mint-meta-"$DESKTOP" # just ensuring this meta package was not uninstalled, it will wait for confirmation if it was
+    install_pkgs mint-meta-codecs
     copy_file /tmp/linuxmint.dconf "${BASE_REPO_LOCATION}"specific/linuxmint.dconf
     dconf load / </tmp/linuxmint.dconf
     rm -f /tmp/linuxmint.dconf
