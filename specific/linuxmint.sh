3a7f98d2006effe9a7efce71adbd217225ffc84e
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-09 12:17:15 +0530

    updated

--- specific/linuxmint.sh
+++ specific/linuxmint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 setup_linuxmint() {
+    sudo sed -i "s/http:\/\/packages.linuxmint.com/https:\/\/fastly.linuxmint.io/g" /etc/apt/sources.list.d/official-package-repositories.list
+    refresh_package_sources
     uninstall_pkgs "hypnotix mintchat pix* sticky thingy *timeshift* warpinator webapp-manager"
     sudo apt-get install mint-meta-"$DESKTOP" # just ensuring this meta package was not uninstalled, it will wait for confirmation if it was
     install_pkgs mint-meta-codecs
