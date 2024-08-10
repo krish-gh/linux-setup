2ff3e594d5a65d432324a7b2fbf30c3d93e932e6
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-10 17:53:46 +0530

    updated

--- desktop/kde.sh
+++ desktop/kde.sh
@@ -6,6 +6,11 @@ GUI_TEXT_EDITOR=org.kde.kwrite.desktop
 setup_kde() {
     echo -e "Configuring kde stuffs..."
     install_pkgs "$KDE_PACKAGES_TO_INSTALL"
+
+    [[ -f ~/.config/environment.d/10-defaults.conf ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/environment.d/10-defaults.conf
+    [[ -f ~/.profile ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.profile
+    [[ -f ~/.xprofile ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.xprofile
+    [[ -f ~/.config/systemd/user/service.d/env.conf ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/systemd/user/service.d/env.conf
 }
 
 echo -e "Done gnome.sh..."
