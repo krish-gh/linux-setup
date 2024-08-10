ed28adee8ba666f7131a7571abe514e01a5928ae
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-10 18:08:15 +0530

    updated

--- desktop/kde.sh
+++ desktop/kde.sh
@@ -7,6 +7,7 @@ setup_kde() {
     echo -e "Configuring kde stuffs..."
     install_pkgs "$KDE_PACKAGES_TO_INSTALL"
 
+    # No need for qt theme in kde environment
     [[ -f ~/.config/environment.d/10-defaults.conf ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/environment.d/10-defaults.conf
     [[ -f ~/.profile ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.profile
     [[ -f ~/.xprofile ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.xprofile
