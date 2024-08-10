751687fd23d2b8a06fe844a268a3447fc4891bbc
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-10 18:06:34 +0530

    updated

--- desktop/kde.sh
+++ desktop/kde.sh
@@ -7,10 +7,10 @@ setup_kde() {
     echo -e "Configuring kde stuffs..."
     install_pkgs "$KDE_PACKAGES_TO_INSTALL"
 
-    [[ -f ~/.config/environment.d/10-defaults.conf ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/environment.d/10-defaults.conf
-    [[ -f ~/.profile ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.profile
-    [[ -f ~/.xprofile ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.xprofile
-    [[ -f ~/.config/systemd/user/service.d/env.conf ]] && sed '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/systemd/user/service.d/env.conf
+    [[ -f ~/.config/environment.d/10-defaults.conf ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/environment.d/10-defaults.conf
+    [[ -f ~/.profile ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.profile
+    [[ -f ~/.xprofile ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.xprofile
+    [[ -f ~/.config/systemd/user/service.d/env.conf ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/systemd/user/service.d/env.conf
 }
 
 echo -e "Done gnome.sh..."
