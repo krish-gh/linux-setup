0228de7b0812d8487e348328ada8727ed45980b9
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-11 09:42:33 +0530

    updated

--- desktop/kde.sh
+++ desktop/kde.sh
@@ -13,37 +13,41 @@ setup_kde() {
     [[ -f ~/.xprofile ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.xprofile
     [[ -f ~/.config/systemd/user/service.d/env.conf ]] && sed -i '/QT_QPA_PLATFORMTHEME/s/^/#/' ~/.config/systemd/user/service.d/env.conf
 
+    KWRITECONFIG_CMD=kwriteconfig6
+    ! command_exists kwriteconfig6 && KWRITECONFIG_CMD=kwriteconfig5
+
     # configs
     lookandfeeltool -a org.kde.breezedark.desktop
-    kwriteconfig6 --file ~/.config/kdeglobals --group KScreen --key ScaleFactor 1.3
-    kwriteconfig6 --file ~/.config/kwinrc --group Xwayland --key Scale 1.3
-    kwriteconfig6 --file ~/.config/plasmashellrc --group PlasmaViews --group "Panel 2" --group Defaults --key thickness 30
+    KWRITECONFIG_CMD --file ~/.config/kdeglobals --group KScreen --key ScaleFactor 1.3
+    KWRITECONFIG_CMD --file ~/.config/kwinrc --group Xwayland --key Scale 1.3
+    KWRITECONFIG_CMD --file ~/.config/plasmashellrc --group PlasmaViews --group "Panel 2" --group Defaults --key thickness 30
+    KWRITECONFIG_CMD --file ~/.config/krunnerrc --group Plugins --key baloosearchEnabled false
 
     # konsole
     copy_file ~/.local/share/konsole/catppuccin-mocha.colorscheme https://raw.githubusercontent.com/catppuccin/konsole/main/themes/catppuccin-mocha.colorscheme
-    kwriteconfig6 --file ~/.local/share/konsole/custom.profile --group Appearance --key ColorScheme catppuccin-mocha
-    kwriteconfig6 --file ~/.local/share/konsole/custom.profile --group Appearance --key Font "JetBrainsMono Nerd Font,12"
-    kwriteconfig6 --file ~/.local/share/konsole/custom.profile --group Appearance --key UseFontLineChararacters true
-    kwriteconfig6 --file ~/.local/share/konsole/custom.profile --group General --key Name custom
-    kwriteconfig6 --file ~/.local/share/konsole/custom.profile --group General --key TerminalColumns 120
-    kwriteconfig6 --file ~/.local/share/konsole/custom.profile --group General --key TerminalRows 36    
-    kwriteconfig6 --file ~/.config/konsolerc --group "Desktop Entry" --key DefaultProfile custom.profile
-    kwriteconfig6 --file ~/.config/konsolerc --group KonsoleWindow --key RememberWindowSize false
-    kwriteconfig6 --file ~/.config/konsolerc --group KonsoleWindow --key UseSingleInstance true
+    KWRITECONFIG_CMD --file ~/.local/share/konsole/custom.profile --group Appearance --key ColorScheme catppuccin-mocha
+    KWRITECONFIG_CMD --file ~/.local/share/konsole/custom.profile --group Appearance --key Font "JetBrainsMono Nerd Font,12"
+    KWRITECONFIG_CMD --file ~/.local/share/konsole/custom.profile --group Appearance --key UseFontLineChararacters true
+    KWRITECONFIG_CMD --file ~/.local/share/konsole/custom.profile --group General --key Name custom
+    KWRITECONFIG_CMD --file ~/.local/share/konsole/custom.profile --group General --key TerminalColumns 120
+    KWRITECONFIG_CMD --file ~/.local/share/konsole/custom.profile --group General --key TerminalRows 36    
+    KWRITECONFIG_CMD --file ~/.config/konsolerc --group "Desktop Entry" --key DefaultProfile custom.profile
+    KWRITECONFIG_CMD --file ~/.config/konsolerc --group KonsoleWindow --key RememberWindowSize false
+    KWRITECONFIG_CMD --file ~/.config/konsolerc --group KonsoleWindow --key UseSingleInstance true
 
     # kate
-    kwriteconfig6 --file ~/.config/katerc --group General --key "Close After Last" true
-    kwriteconfig6 --file ~/.config/katerc --group General --key "Restore Window Configuration" false
-    kwriteconfig6 --file ~/.config/katerc --group General --key "Save Meta Infos" false
-    kwriteconfig6 --file ~/.config/katerc --group "KTextEditor Renderer" --key "Color Theme" "Catppuccin Mocha"
-    kwriteconfig6 --file ~/.config/katerc --group "KTextEditor Renderer" --key "Text Font" "JetBrainsMono Nerd Font,12"
+    KWRITECONFIG_CMD --file ~/.config/katerc --group General --key "Close After Last" true
+    KWRITECONFIG_CMD --file ~/.config/katerc --group General --key "Restore Window Configuration" false
+    KWRITECONFIG_CMD --file ~/.config/katerc --group General --key "Save Meta Infos" false
+    KWRITECONFIG_CMD --file ~/.config/katerc --group "KTextEditor Renderer" --key "Color Theme" "Catppuccin Mocha"
+    KWRITECONFIG_CMD --file ~/.config/katerc --group "KTextEditor Renderer" --key "Text Font" "JetBrainsMono Nerd Font,12"
 
     # kwrite
-    kwriteconfig6 --file ~/.config/kwriterc --group General --key "Close After Last" true
-    kwriteconfig6 --file ~/.config/kwriterc --group General --key "Restore Window Configuration" false
-    kwriteconfig6 --file ~/.config/kwriterc --group General --key "Save Meta Infos" false
-    kwriteconfig6 --file ~/.config/kwriterc --group "KTextEditor Renderer" --key "Color Theme" "Catppuccin Mocha"
-    kwriteconfig6 --file ~/.config/kwriterc --group "KTextEditor Renderer" --key "Text Font" "JetBrainsMono Nerd Font,12"
+    KWRITECONFIG_CMD --file ~/.config/kwriterc --group General --key "Close After Last" true
+    KWRITECONFIG_CMD --file ~/.config/kwriterc --group General --key "Restore Window Configuration" false
+    KWRITECONFIG_CMD --file ~/.config/kwriterc --group General --key "Save Meta Infos" false
+    KWRITECONFIG_CMD --file ~/.config/kwriterc --group "KTextEditor Renderer" --key "Color Theme" "Catppuccin Mocha"
+    KWRITECONFIG_CMD --file ~/.config/kwriterc --group "KTextEditor Renderer" --key "Text Font" "JetBrainsMono Nerd Font,12"
     
 
 }
