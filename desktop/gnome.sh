16a2571671fa7fd8e055255e7cd427df8c6c8165
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-14 11:29:46 +0530

    updated

--- desktop/gnome.sh
+++ desktop/gnome.sh
@@ -14,7 +14,7 @@ setup_gnome() {
 
     echo -e "Installing some extensions..."
     if command_exists flatpak; then
-        flatpak install --user flathub com.mattjakeman.ExtensionManager --assumeyes
+        flatpak install --assumeyes flathub com.mattjakeman.ExtensionManager
     else
         install_pkgs "$GNOME_EXT_MGR_PKG"
     fi
