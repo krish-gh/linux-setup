ab1173c43f37e65b87e3a6778dfd2802e410f669
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-20 02:09:34 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -6,6 +6,11 @@ GUI_TEXT_EDITOR=org.xfce.mousepad.desktop
 setup_xfce() {
     echo -e "Configuring xfce stuffs..."
     install_pkgs "$XFCE_PACKAGES_TO_INSTALL"
+
+    # xfce4-terminal
+    mkdir -p ~/.local/share/xfce4/terminal/colorschemes
+    copy_file ~/.local/share/xfce4/terminal/colorschemes/catppuccin-mocha.theme https://raw.githubusercontent.com/catppuccin/xfce4-terminal/main/themes/catppuccin-mocha.theme
+
 }
 
 echo -e "Done xfce.sh..."
