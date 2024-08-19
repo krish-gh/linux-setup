130db77c9ab5f63a72f1cc990c04fa1f56aa83c0
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-20 02:48:37 +0530

    updated

--- specific/arch.sh
+++ specific/arch.sh
@@ -2,6 +2,11 @@
 
 setup_specific_arch_xfce() {
     install_pkgs "materia-gtk-theme papirus-icon-theme"
+    gsettings set org.gnome.desktop.interface gtk-theme Materia-dark
+    gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
+    xfconf-query -c xsettings -p /Net/ThemeName -s Materia-dark
+    xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
+    xfconf-query -c xfwm4 -p /general/theme -s Materia-dark
 }
 
 echo -e "Done arch.sh..."
