7a98cbc56ebc359aef5af669c37623f6f85f7707
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-20 02:33:24 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -11,6 +11,9 @@ setup_xfce() {
     mkdir -p ~/.local/share/xfce4/terminal/colorschemes
     copy_file ~/.local/share/xfce4/terminal/colorschemes/catppuccin-mocha.theme https://raw.githubusercontent.com/catppuccin/xfce4-terminal/main/themes/catppuccin-mocha.theme
 
+    copy_file "$TEMP_DIR"/xfce.dconf "${BASE_REPO_LOCATION}"desktop/xfce.dconf
+    dconf load / <"$TEMP_DIR"/xfce.dconf
+    rm -f "$TEMP_DIR"/xfce.dconf
 }
 
 echo -e "Done xfce.sh..."
