7638ba915de168cc702250c02ac5e9bd142b77e6
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-20 10:01:45 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -8,17 +8,22 @@ setup_xfce() {
     install_pkgs "$XFCE_PACKAGES_TO_INSTALL"
 
     # config
-    xfconf-query -c xsettings -p /Xft/DPI -s 136
-    xfconf-query -c xsettings -p /Xft/Hinting -s 1
-    xfconf-query -c xsettings -p /Xft/HintStyle -s hintslight
-    xfconf-query -c xsettings -p /Xft/RGBA -s rgb
-    xfconf-query -c xsettings -p /Xfce/LastCustomDPI -s 136
-    xfconf-query -c xsettings -p /Xfce/SyncThemes -s true 
+    xfconf-query -c xsettings -v --create -p /Xft/DPI -t int -s 136
+    xfconf-query -c xsettings -v --create -p /Xft/Hinting -t int -s 1
+    xfconf-query -c xsettings -v --create -p /Xft/HintStyle -t string -s hintslight
+    xfconf-query -c xsettings -v --create -p /Xft/RGBA -t string -s rgb
+    xfconf-query -c xsettings -v --create -p /Xfce/LastCustomDPI -t int -s 136
+    xfconf-query -c xsettings -v --create -p /Xfce/SyncThemes -t bool -s true 
 
     # xfce4-terminal
     mkdir -p ~/.local/share/xfce4/terminal/colorschemes
     copy_file ~/.local/share/xfce4/terminal/colorschemes/catppuccin-mocha.theme https://raw.githubusercontent.com/catppuccin/xfce4-terminal/main/themes/catppuccin-mocha.theme
     copy_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml "${BASE_REPO_LOCATION}"home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
+
+    # thunar
+    xfconf-query -c thunar -v --create -p /last-show-hidden -t bool -s true
+    xfconf-query -c thunar -v --create -p /misc-thumbnail-mode -t string -s THUNAR_THUMBNAIL_MODE_NEVER
+    xfconf-query -c thunar -v --create -p /misc-thumbnail-max-file-size -t uint64 -s 1
 
     copy_file "$TEMP_DIR"/xfce.dconf "${BASE_REPO_LOCATION}"desktop/xfce.dconf
     dconf load / <"$TEMP_DIR"/xfce.dconf
--- specific/arch.sh
+++ specific/arch.sh
@@ -4,9 +4,9 @@ setup_specific_arch_xfce() {
     install_pkgs "materia-gtk-theme papirus-icon-theme"
     gsettings set org.gnome.desktop.interface gtk-theme Materia-dark
     gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
-    xfconf-query -c xsettings -p /Net/ThemeName -s Materia-dark
-    xfconf-query -c xsettings -p /Net/IconThemeName -s Papirus-Dark
-    xfconf-query -c xfwm4 -p /general/theme -s Materia-dark
+    xfconf-query -c xsettings -v --create -p /Net/ThemeName -t string -s Materia-dark
+    xfconf-query -c xsettings -v --create -p /Net/IconThemeName -t string -s Papirus-Dark
+    xfconf-query -c xfwm4 -v --create -p /general/theme -t string -s Materia-dark
 }
 
 echo -e "Done arch.sh..."
