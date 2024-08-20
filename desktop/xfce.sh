ae0c4b0ac095e0bd05639fa18c54c29776dd1ee5
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-20 09:42:23 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -13,12 +13,12 @@ setup_xfce() {
     xfconf-query -c xsettings -p /Xft/HintStyle -s hintslight
     xfconf-query -c xsettings -p /Xft/RGBA -s rgb
     xfconf-query -c xsettings -p /Xfce/LastCustomDPI -s 136
-    xfconf-query -c xsettings -p /Xfce/SyncThemes -s true    
-
+    xfconf-query -c xsettings -p /Xfce/SyncThemes -s true 
 
     # xfce4-terminal
     mkdir -p ~/.local/share/xfce4/terminal/colorschemes
     copy_file ~/.local/share/xfce4/terminal/colorschemes/catppuccin-mocha.theme https://raw.githubusercontent.com/catppuccin/xfce4-terminal/main/themes/catppuccin-mocha.theme
+    copy_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml "${BASE_REPO_LOCATION}"home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
 
     copy_file "$TEMP_DIR"/xfce.dconf "${BASE_REPO_LOCATION}"desktop/xfce.dconf
     dconf load / <"$TEMP_DIR"/xfce.dconf
