697456b9080e86079d5e1269d0084c2c54478f71
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-20 10:40:38 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -8,14 +8,14 @@ setup_xfce() {
     install_pkgs "$XFCE_PACKAGES_TO_INSTALL"
 
     # config
-    xfconf-query -c xsettings -v --create -p /Xft/DPI -t int -s 136
-    xfconf-query -c xsettings -v --create -p /Xft/Hinting -t int -s 1
-    xfconf-query -c xsettings -v --create -p /Xft/HintStyle -t string -s hintslight
-    xfconf-query -c xsettings -v --create -p /Xft/RGBA -t string -s rgb
-    xfconf-query -c xsettings -v --create -p /Xfce/LastCustomDPI -t int -s 136
-    xfconf-query -c xsettings -v --create -p /Xfce/SyncThemes -t bool -s true 
-    xfconf-query -c xfce4-session -v --create -p /general/SaveOnExit -t bool -s false 
-    xfconf-query -c xfce4-session -v --create -p /compat/LaunchGNOME -t bool -s true 
+    xfconf-query -c xsettings -v -n -p /Xft/DPI -t int -s 136
+    xfconf-query -c xsettings -v -n -p /Xft/Hinting -t int -s 1
+    xfconf-query -c xsettings -v -n -p /Xft/HintStyle -t string -s hintslight
+    xfconf-query -c xsettings -v -n -p /Xft/RGBA -t string -s rgb
+    xfconf-query -c xsettings -v -n -p /Xfce/LastCustomDPI -t int -s 136
+    xfconf-query -c xsettings -v -n -p /Xfce/SyncThemes -t bool -s true 
+    xfconf-query -c xfce4-session -v -n -p /general/SaveOnExit -t bool -s false 
+    xfconf-query -c xfce4-session -v -n -p /compat/LaunchGNOME -t bool -s true 
 
     # xfce4-terminal
     mkdir -p ~/.local/share/xfce4/terminal/colorschemes
@@ -23,9 +23,9 @@ setup_xfce() {
     copy_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml "${BASE_REPO_LOCATION}"home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
 
     # thunar
-    xfconf-query -c thunar -v --create -p /last-show-hidden -t bool -s true
-    xfconf-query -c thunar -v --create -p /misc-thumbnail-mode -t string -s THUNAR_THUMBNAIL_MODE_NEVER
-    xfconf-query -c thunar -v --create -p /misc-thumbnail-max-file-size -t uint64 -s 1
+    xfconf-query -c thunar -v -n -p /last-show-hidden -t bool -s true
+    xfconf-query -c thunar -v -n -p /misc-thumbnail-mode -t string -s THUNAR_THUMBNAIL_MODE_NEVER
+    xfconf-query -c thunar -v -n -p /misc-thumbnail-max-file-size -t uint64 -s 1
 
     copy_file "$TEMP_DIR"/xfce.dconf "${BASE_REPO_LOCATION}"desktop/xfce.dconf
     dconf load / <"$TEMP_DIR"/xfce.dconf
--- specific/arch.sh
+++ specific/arch.sh
@@ -4,9 +4,9 @@ setup_specific_arch_xfce() {
     install_pkgs "materia-gtk-theme papirus-icon-theme"
     gsettings set org.gnome.desktop.interface gtk-theme Materia-dark
     gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
-    xfconf-query -c xsettings -v --create -p /Net/ThemeName -t string -s Materia-dark
-    xfconf-query -c xsettings -v --create -p /Net/IconThemeName -t string -s Papirus-Dark
-    xfconf-query -c xfwm4 -v --create -p /general/theme -t string -s Materia-dark
+    xfconf-query -c xsettings -v -n -p /Net/ThemeName -t string -s Materia-dark
+    xfconf-query -c xsettings -v -n -p /Net/IconThemeName -t string -s Papirus-Dark
+    xfconf-query -c xfwm4 -v -n -p /general/theme -t string -s Materia-dark
 }
 
 echo -e "Done arch.sh..."
