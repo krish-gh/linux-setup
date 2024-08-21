7f9f87583a020bde97af550fbbf32fbed84f6db8
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-22 01:08:55 +0530

    updated

--- specific/arch.sh
+++ specific/arch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 setup_specific_arch_xfce() {
-    setup_xfce_panel
     install_pkgs "materia-gtk-theme papirus-icon-theme"
     gsettings set org.gnome.desktop.interface gtk-theme Materia-dark
     gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
@@ -9,6 +8,7 @@ setup_specific_arch_xfce() {
     xfconf-query -c xsettings -v -n -p /Net/IconThemeName -t string -s Papirus-Dark
     xfconf-query -c xfwm4 -v -n -p /general/theme -t string -s Default-hdpi
     set_wallpaper "/usr/share/backgrounds/xfce/Aquarius.svg"
+    setup_xfce_panel
 }
 
 echo -e "Done arch.sh..."
