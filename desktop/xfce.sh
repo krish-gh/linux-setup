8ae1b2e30e24149a8b0c62c27570266c4afa6731
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-21 16:41:54 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -57,4 +57,9 @@ setup_xfce_panel() {
     xfce4-panel &
 }
 
+set_wallpaper() {
+    # shellcheck disable=SC2046
+    xfconf-query -c xfce4-desktop -p $(xfconf-query -c xfce4-desktop -l | grep "workspace0/last-image") -s "$1"
+}
+
 echo -e "Done xfce.sh..."
