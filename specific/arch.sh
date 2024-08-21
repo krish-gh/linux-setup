8bcbc438b0c47f12f4a62aef1cac045d36837df6
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-21 16:14:11 +0530

    updated

--- specific/arch.sh
+++ specific/arch.sh
@@ -7,7 +7,7 @@ setup_specific_arch_xfce() {
     gsettings set org.gnome.desktop.interface icon-theme Papirus-Dark
     xfconf-query -c xsettings -v -n -p /Net/ThemeName -t string -s Materia-dark
     xfconf-query -c xsettings -v -n -p /Net/IconThemeName -t string -s Papirus-Dark
-    xfconf-query -c xfwm4 -v -n -p /general/theme -t string -s Materia-dark
+    xfconf-query -c xfwm4 -v -n -p /general/theme -t string -s Default-hdpi
 }
 
 echo -e "Done arch.sh..."
