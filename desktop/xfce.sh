0191619a3681a05305b7bb1f50bee94a8e5b7c2d
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-22 07:21:41 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -54,8 +54,9 @@ setup_xfce_panel() {
     rm -rf ~/.config/xfce4/panel/launcher-*
     mkdir -p ~/.config/xfce4/panel/launcher-{2,3,4}
     copy_file ~/.config/xfce4/panel/launcher-2/FileManager.desktop "${BASE_REPO_LOCATION}"home/.config/xfce4/panel/launcher-2/FileManager.desktop
-    copy_file ~/.config/xfce4/panel/launcher-3/TerminalEmulator.desktop "${BASE_REPO_LOCATION}"home/.config/xfce4/panel/launcher-3/TerminalEmulator.desktop
-    copy_file ~/.config/xfce4/panel/launcher-4/WebBrowser.desktop "${BASE_REPO_LOCATION}"home/.config/xfce4/panel/launcher-4/WebBrowser.desktop
+    copy_file ~/.config/xfce4/panel/launcher-3/TextEditor.desktop "${BASE_REPO_LOCATION}"home/.config/xfce4/panel/launcher-3/TextEditor.desktop
+    copy_file ~/.config/xfce4/panel/launcher-4/TerminalEmulator.desktop "${BASE_REPO_LOCATION}"home/.config/xfce4/panel/launcher-4/TerminalEmulator.desktop
+    copy_file ~/.config/xfce4/panel/launcher-5/WebBrowser.desktop "${BASE_REPO_LOCATION}"home/.config/xfce4/panel/launcher-5/WebBrowser.desktop
     copy_file ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml "${BASE_REPO_LOCATION}"home/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-panel.xml
     xfce4-panel &
 }
