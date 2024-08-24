53652d4294a862670df4c92e81651280f926406d
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-24 09:07:13 +0530

    updated

--- specific/debian.sh
+++ specific/debian.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 setup_specific_debian_xfce() {
-    install_pkgs "atril ristretto materia-gtk-theme papirus-icon-theme"
+    install_pkgs "slick-greeter atril ristretto materia-gtk-theme papirus-icon-theme"
     setup_xfce_theme
     setup_xfce_panel
 }
