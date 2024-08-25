b73eb0e01005bbcf1d4a9d8c065d39326d22e0bf
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-26 01:30:15 +0530

    updated

--- desktop/xfce.sh
+++ desktop/xfce.sh
@@ -80,6 +80,9 @@ setup_xfce_theme() {
     xfconf-query -c xsettings -v -n -p /Net/ThemeName -t string -s Materia-dark
     xfconf-query -c xsettings -v -n -p /Net/IconThemeName -t string -s Papirus-Dark
     xfconf-query -c xfwm4 -v -n -p /general/theme -t string -s Default-hdpi
+    echo -e "[Greeter]" | sudo tee /etc/lightdm/slick-greeter.conf
+    echo -e "theme-name=Materia-dark" | sudo tee -a /etc/lightdm/slick-greeter.conf
+    echo -e "icon-theme-name=Papirus-Dark" | sudo tee -a /etc/lightdm/slick-greeter.conf
 }
 
 set_xfce_wallpaper() {
