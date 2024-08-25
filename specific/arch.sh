309cd777ab3c85d0b6a5531f825f9c361212cb20
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-25 11:37:11 +0530

    updated

--- specific/arch.sh
+++ specific/arch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 setup_specific_arch_xfce() {
+    install_pkgs "materia-gtk-theme papirus-icon-theme"
     setup_xfce_theme
     set_xfce_wallpaper "/usr/share/backgrounds/xfce/Aquarius.svg"
     setup_xfce_panel
