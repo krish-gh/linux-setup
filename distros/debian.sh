652a6608020d4d318b7f849486ca40672659fbb4
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-25 21:20:40 +0530

    updated

--- distros/debian.sh
+++ distros/debian.sh
@@ -8,7 +8,7 @@ UNINSTALL_CMD="sudo apt-get purge --ignore-missing --auto-remove -y"
 UNINSTALL_ONLY_CMD="sudo apt-get purge --ignore-missing -y"
 
 REQUIREMENTS="curl wget unzip dconf-cli jq"
-SYSTEM_PACKAGES_TO_INSTALL="fwupd mesa-vulkan-drivers firmware-sof-signed alsa-firmware-loaders fprintd libpam-fprintd power-profiles-daemon"
+SYSTEM_PACKAGES_TO_INSTALL="fwupd mesa-vulkan-drivers alsa-firmware-loaders firmware-sof-signed fprintd libpam-fprintd power-profiles-daemon"
 INTEL_PACKAGES_TO_INSTALL="intel-media-va-driver-non-free va-driver-all"
 VMWARE_PACKAGES_TO_INSTALL="xserver-xorg-video-vmware xserver-xorg-video-qxl open-vm-tools-desktop"
 VBOX_PACKAGES_TO_INSTALL="virtualbox-guest-x11"
