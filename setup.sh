7e84dfa4a5bd15b1cad3e2d0f4a8e321025e437c
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-27 21:25:27 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 # shellcheck disable=SC2046
 
 ## option #1
-eval "$(curl -fsSL https://raw.githubusercontent.com/krish-gh/linux-setup/main/scripts/setup-main.sh?$(date +%s))" 2>&1 | tee setup-$(date '+%Y-%m-%d-%H:%M:%S').log
+timestamp=$(date '+%Y-%m-%d-%H:%M:%S')
+eval "$(curl -fsSL https://raw.githubusercontent.com/krish-gh/linux-setup/main/scripts/setup-main.sh?"$timestamp")" 2>&1 | tee setup-"$timestamp".log
