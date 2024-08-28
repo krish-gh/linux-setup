2eca74dc1c4bdb91ff12c3a8d3451239d9d3a321
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-28 10:44:17 +0530

    updated

--- specific/debian.sh
+++ specific/debian.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+setup_specific_debian() {
+    install_pkgs pipewire-audio
+}
+
 setup_specific_debian_xfce() {
     install_pkgs "atril ristretto materia-gtk-theme papirus-icon-theme"
     setup_xfce_theme
