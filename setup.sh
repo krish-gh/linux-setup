8eff51eff0af12e4d4b43e2da597ccbcd3d798a1
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-31 16:17:39 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # shellcheck disable=SC1091
+# shellcheck disable=SC2128
 
-chmod +x scripts/setup-main.sh &&
+scriptDir=$(dirname -- "$(readlink -f -- "$BASH_SOURCE")")
+chmod +x "$scriptDir"/scripts/setup-main.sh &&
 timestamp=$(date '+%Y-%m-%d-%H:%M:%S') &&
-. scripts/setup-main.sh 2>&1 | tee ~/setup-"$timestamp".log 
+. "$scriptDir"/scripts/setup-main.sh 2>&1 | tee ~/setup-"$timestamp".log 
