9009373c16232ea1ecec9afaf412f8d100119187
krish-gh <2088089+krish-gh@users.noreply.github.com>
2024-08-31 13:54:40 +0530

    updated

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # shellcheck disable=SC2046
 # shellcheck disable=SC1091
+# shellcheck disable=SC2164
+# shellcheck disable=SC2103
 
 ## option #1
 timestamp=$(date '+%Y-%m-%d-%H:%M:%S')
@@ -8,9 +10,9 @@ eval "$(curl -fsSL https://raw.githubusercontent.com/krish-gh/linux-setup/main/s
 
 ## option #2
 git clone https://github.com/krish-gh/linux-setup.git
-cd linux-setup || exit
+cd linux-setup
 timestamp=$(date '+%Y-%m-%d-%H:%M:%S')
 chmod +x scripts/setup-main.sh
 . scripts/setup-main.sh 2>&1 | tee setup-"$timestamp".log
-cd .. || exit
+cd ..
 rm -r linux-setup
